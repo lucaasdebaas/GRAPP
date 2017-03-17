f1ab52c7886715624da5847f8e83930a6af062cf
ArendJanQ <arendjanquist@gmail.com>
2017-03-17 11:48:37 +0100

    Update calculatie.js

--- calculatie.js
+++ calculatie.js
@@ -4,7 +4,7 @@ var ans="";
 function calculate() {
 	var berekening = invoer;
 	for (i=0; i<tekens; i++) {
-		if (berekening.slice(i,i+2) == "*/" || berekening.slice(i,i+2) == "/*" || berekening.slice(i,i+2) == "*+" || berekening.slice(i,i+2) == "/+" || berekening.slice(i,i+2) == "-+" || berekening.slice(i,i+2) == "-*" || berekening.slice(i,i+2) == "+*" || berekening.slice(i,i+2) == "-/" || berekening.slice(i,i+2) == "+/" || berekening.slice(i,i+2) == "**" || berekening.slice(i,i+2) == "//") {
+		if (berekening.slice(i,i+2) == "*/" || berekening.slice(i,i+2) == "/*" || berekening.slice(i,i+2) == "*+" || berekening.slice(i,i+2) == "/+" || berekening.slice(i,i+2) == "-+" || berekening.slice(i,i+2) == "-*" || berekening.slice(i,i+2) == "+*" || berekening.slice(i,i+2) == "-/" || berekening.slice(i,i+2) == "+/" || berekening.slice(i,i+2) == "**" || berekening.slice(i,i+2) == "//" || berekening.slice(i,i+2) == "++") {
 		    berekening = "error";  //De variabele berekening is error, zodat de variabele uitvoer undefined zal zijn.
 		}
 	}
